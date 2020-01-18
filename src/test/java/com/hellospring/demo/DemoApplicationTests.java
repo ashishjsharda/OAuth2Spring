2a99f6455c467515045e9c322713400f9849f501
Ashish Sharda <ashishjsharda@gmail.com>
2020-01-18 09:57:49 -0500

    Add all my files

---
+++ src/test/java/com/hellospring/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hellospring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
